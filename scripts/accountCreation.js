e863dcdba6f5ff6c9602ff3b8e9e2c6d720cbb1d
fargila <atilahunesco@gmail.com>
2024-09-08 19:53:27 -0300

    :broom: cleanup: Removendo código desnecessário no 'accountCreation.js'

--- scripts/accountCreation.js
+++ scripts/accountCreation.js
@@ -7,27 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
     form.addEventListener('submit', (e) => {
         let messages = []
-
-        if (name.value.trim() === '') {
-            messages.push('Name is required')
-        }
-        
-        if (email.value.trim() === '') {
-            messages.push('Email is required')
+    
+        const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).{8,}$/
+        if (!passwordRegex.test(password.value)) {
+            messages.push('Password must be at least 8 characters long, include at least one number and one special character')
         }
 
-        if (password.value.trim() === '') {
-            messages.push('Password is required')
-        } else {
-            const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).{8,}$/
-            if (!passwordRegex.test(password.value)) {
-                messages.push('Password must be at least 8 characters long, include at least one number and one special character')
-            }
-        }
-
-        if (confirmPassword.value.trim() === '') {
-            messages.push('Confirm Password is required')
-        } else if (confirmPassword.value !== password.value) {
+        if (confirmPassword.value !== password.value) {
             messages.push('Passwords do not match')
         }
 
